6b4efdb75fb76425d689df89d80925fb1e0095e1
Amit Kumar <138309651+officialamit558@users.noreply.github.com>
2024-01-21 19:55:57 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -2,14 +2,14 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s[APMapm]{2}\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
     # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %I:%M %p - ')
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%M/%D/%Y - ')
 
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
